5abe01b92c5af422dce161d2c54ff343b05c542b
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2026-09-25 15:34:02 -0500

    fix vite base path
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- chess-system-one/vite.config.ts
+++ chess-system-one/vite.config.ts
@@ -3,13 +3,14 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/chess-system-one/",
   plugins: [vue()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
+      "/api": {
+        target: "http://localhost:8000",
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
     },
-  }
-})
+  },
+});
